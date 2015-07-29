9dd782a4a7491dec10aae9f73fbd81a6e2f2eeaa
Paul Vincent Beigang <pbeigang@gmail.com>
2015-07-29 23:50:30 +0200

    Move action before cart unset and add two parameters.

--- includes/class-woocommerce-cart-stock-reducer.php
+++ includes/class-woocommerce-cart-stock-reducer.php
@@ -112,8 +112,8 @@ class WC_Cart_Stock_Reducer extends WC_Integration {
 		if ( 'yes' === $this->expire_items ) {
 			$expired_cart_notice = apply_filters( 'wc_csr_expired_cart_notice', sprintf( __( "Sorry, '%s' was removed from your cart because you didn't checkout before the expiration time.", 'woocommerce-cart-stock-reducer' ), $cart->cart_contents[ $cart_id ][ 'data' ]->get_title() ), $cart_id, $cart );
 			wc_add_notice( $expired_cart_notice, 'error' );
+			do_action( 'wc_csr_before_remove_expired_item', $cart_id, $cart );
 			unset( $cart->cart_contents[ $cart_id ] );
-			do_action( 'wc_csr_after_removed_expired_item' );
 		}
 
 	}
