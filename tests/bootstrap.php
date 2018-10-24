4b45bb93f5d0db4e26689a00cde38cabe9da32b8
James Golovich <james@gnuinter.net>
2018-10-24 15:25:22 -0700

    Change bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,16 +15,23 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
 }
 
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+// Code from https://github.com/liquidweb/woocommerce-custom-orders-table
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/woocommerce-cart-stock-reducer.php';
+$_bootstrap = dirname( __DIR__ ) . '/vendor/woocommerce/woocommerce/tests/bootstrap.php';
+
+// Verify that Composer dependencies have been installed.
+if ( ! file_exists( $_bootstrap ) ) {
+	echo "\033[0;31mUnable to find the WooCommerce test bootstrap file. Have you run `composer install`?\033[0;m" . PHP_EOL;
+	exit( 1 );
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Finally, Start up the WP testing environment.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+require_once $_bootstrap;
+
+// Give access to tests_add_filter() function.
+//require_once $_tests_dir . '/includes/functions.php';
+require dirname( dirname( __FILE__ ) ) . '/woocommerce-cart-stock-reducer.php';
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+//require $_tests_dir . '/includes/bootstrap.php';
