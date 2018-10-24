ab40c3bae903c692828cb9033e410a58c9dd294e
James Golovich <james@gnuinter.net>
2018-10-24 15:13:08 -0700

    Change to using Woo test base

--- tests/test-woocommerce.php
+++ tests/test-woocommerce.php
@@ -8,7 +8,7 @@
 /**
  * Test for WooCommerce.
  */
-class WooCommerceTest extends WP_UnitTestCase {
+class WooCommerceTest extends WC_Unit_Test_Case {
 
 	/**
 	 * Test if WooCommerce version is set.
